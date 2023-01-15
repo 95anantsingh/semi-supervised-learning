9333eceaa174a20c85defbecc59257b3304bda69
Anant <95anantsingh@gmail.com>
2023-01-15 04:58:49 -0500

    maximize button

--- assets/acc_comp.js
+++ assets/acc_comp.js
@@ -2,13 +2,15 @@
 var dataset = "dbpedia_14";
 var strategy = "top_k";
 var sampling = "";
-var plotly_html = document.getElementById('plotly_html');
+
+var plotly_div = document.getElementById('plotly_div');
+var plotly_iframe = document.getElementById('plotly_iframe');
 var dataset_selector = document.getElementById("dataset_selector");
 var strategy_selector = document.getElementById("strategy_selector");
 var sampling_selector = document.getElementById("sampling_selector");
 var header = document.getElementById("header_");
 var selectors = document.getElementById("selectors");
-
+var maximize_button = document.getElementById('maximize_button');
 
 function strategy_change(){
     strategy = strategy_selector.options[strategy_selector.selectedIndex].value;
@@ -39,10 +41,43 @@ var plot_dir = "";
 var data = '';
 var padding = 0;
 function update_chart(){
-    iframe_height = window.innerHeight;
+    // iframe_height = window.innerHeight;
     plot="visuals/"+dataset+"/"+sampling+strategy+'/acc_comparison.html'; 
-    plotly_html.style.height=iframe_height+'px';  
-    plotly_html.src= plot;
+    // plotly_html.style.height=iframe_height+'px';  
+    plotly_iframe.src= plot;
+    // plotly_html.width=screen.width;
 }
 
-refresh_data();
+
+// Maximize plot
+var scrollTop = 0;
+var scrollLeft = 0;
+var maximized = false;
+function maximize() {
+    if (maximized){
+        maximize_button.style.backgroundColor='white';
+        maximize_button.style.color='black';
+        plotly_div.classList.remove('maximize');
+        window.scrollTo(scrollLeft, scrollTop);
+        maximized = false;
+    }
+    else{
+        maximize_button.style.backgroundColor='indianred';
+        maximize_button.style.color='white';
+        scrollTop =  window.pageYOffset || document.documentElement.scrollTop;
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+        plotly_div.classList.add('maximize');
+        plotly_iframe.style.height=(window.innerHeight-5)+'px';
+        maximized = true;
+    }
+}
+
+// refresh_data();
+
+
+// Style iframe
+function style_iframe(){
+    iframe = window.frames[0].document;
+    // iframe.body.style.margin = 0;
+    // iframe.svg.setAttribute('width','100%');
+}
